2f618b6721073e2f74dbef220817cffb23ae3e3e
Alysson-Chrysthian <alyssonchrysthian@gmail.com>
2025-04-02 20:55:17 -0300

    library updated

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -10,8 +10,9 @@
     @vite('resources/css/app.css')
 
     {{-- External libraries CDN --}}
-    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-
+    <script type="module" src="https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.esm.js"></script>
+    <script nomodule src="https://cdn.jsdelivr.net/npm/@ionic/core/dist/ionic/ionic.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@ionic/core/css/ionic.bundle.css" />
 </head>
 <body>
 
